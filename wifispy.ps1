3201fcf1eb62a20ffd023c04a715fde74c38b76e
8bitmime <dwayne.cyr@gmail.com>
2019-04-04 14:45:30 -0400

    Cleanedup Formatting.

--- wifispy.ps1
+++ wifispy.ps1
@@ -6,63 +6,50 @@ function wifispy
                 $number++
                 Get-Process | netsh wlan show networks mode=bssid > file.txt         # save command output to text file
          }
-    
+
          $lines = 0
          $SSID = Select-String -Path .\file.txt -pattern SSID 
          ForEach ($line in Get-Content "file.txt")        #reads each line of text file
          {
-       
+
              if ($line -match "SSID" -and $line -notmatch "BSSID" )       #get SSIDs
-             { 
-            
+             {
                   $line = $line.Split(":")
-                  $SSID = $line[1].Trim()                 # removes white extra spaces 
-                  
-          
+                  $SSID = $line[1].Trim()                 # removes white extra spaces
              }
-       
+
              Elseif ($line -match "Network type")          #filters Network type
              {
-       
+
                   $line = $line.split(":")
-                  $network = $line[1].Trim()      
-               
-                   
+                  $network = $line[1].Trim()     
              }
-       
+
              Elseif ($line -match "Authentication")          #filters Authentication
              {
-       
+
                   $line = $line.split(":")
-                  $Authen = $line[1].Trim()      
-            
-                   
+                  $Authen = $line[1].Trim()     
              }
-       
+
              Elseif ($line -match "Encryption")
              {
-       
-                  $line = $line.split(":")
-                  $Encryp = $line[1].Trim()      
 
-                   
+                  $line = $line.split(":")
+                  $Encryp = $line[1].Trim()     
              }
-       
+
             Elseif ($line -match "BSSID")           #filters BSSID 
             {
-                
+
                   $line = $line.split()[-1]
-                  $BSSID = $line.Trim()      
-           
+                  $BSSID = $line.Trim()     
                   $a = Get-Date          
                   $date = $a.ToShortDateString()       #  current date
                   $time = $a.ToShortTimeString()       #  current time
                   $SourceIP = ((ipconfig | findstr [0-9].\.)[0]).Split()[-1]     #Host IP 
-                  $SourceName = $env:COMPUTERNAME        #Hostname 
-             
-                   
+                  $SourceName = $env:COMPUTERNAME        #Hostname
              }
-       
+
           }
 }
-    
